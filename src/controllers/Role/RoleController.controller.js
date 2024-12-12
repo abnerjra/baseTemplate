0131a5fb33ac6f4804a9fac0a5b1dd59b69de452
abner.jurado <abner.jurado@teit.cfe.mx>
2024-12-12 10:51:35 -0600

    refactor: changed method names and added per-path permission validation

--- src/controllers/Role/RoleController.controller.js
+++ src/controllers/Role/RoleController.controller.js
@@ -1,35 +1,51 @@
+import BaseController from "../Base/BaseController.controller.js"
+
 import CreateRole from "../../use-cases/Role/CreateRole.usecase.js"
 import GetRole from "../../use-cases/Role/GetRole.usecase.js"
 import ListRole from "../../use-cases/Role/ListRole.usecase.js"
 import UpdateRole from "../../use-cases/Role/UpdateRole.usecase.js"
 
-class RoleController {
+import { error } from "../../helpers/ResponseHandler.helper.js"
+
+class RoleController extends BaseController {
     constructor() {
+        super()
+
         this.listRoleUC = new ListRole
         this.getRoleUC = new GetRole
         this.createRoleUC = new CreateRole
         this.updateRoleUC = new UpdateRole
     }
 
-    listRoles = async (req, res) => {
+    index = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+
         return await this.listRoleUC.handleAll(req, res)
     }
 
-    getRoleById = async (req, res) => {
+    show = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+
         const id = parseInt(req.params.id)
-        
         return await this.getRoleUC.handleGetRoleById(id, res)
     }
 
-    createRole = async (req, res) => {
+    create = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+
         return await this.createRoleUC.handleCreate(req, res)
     }
 
-    updateRole = async (req, res) => {
+    update = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+
         const id = parseInt(req.params.id)
         return await this.updateRoleUC.handleUpdate(id, req, res)
     }
-    
 }
 
 export default RoleController
--- src/controllers/User/UserController.controller.js
+++ src/controllers/User/UserController.controller.js
@@ -1,31 +1,49 @@
+import BaseController from "../Base/BaseController.controller.js"
+
 import CreateUser from "../../use-cases/User/CreateUser.usecase.js"
 import ListUser from "../../use-cases/User/ListUser.usecase.js"
 import GetUser from "../../use-cases/User/GetUser.usecase.js"
 import UpdateUserUseCase from "../../use-cases/User/UpdateUser.usecase.js"
 
-class UserController {
+import { error } from "../../helpers/ResponseHandler.helper.js"
+
+class UserController extends BaseController {
     constructor() {
+        super()
+
         this.listUserUC = new ListUser()
         this.createUserUC = new CreateUser()
         this.getUserUC = new GetUser()
         this.updateUserUC = new UpdateUserUseCase()
     }
 
-    listUsers = async (req, res) => {
+    index = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+        
         return await this.listUserUC.handleAll(req, res)
     }
 
-    getUserById = async (req, res) => {
+    show = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+        
         const id = parseInt(req.params.id)
         const response = await this.getUserUC.handleUserById(id, res)
         return response
     }
 
-    createUser = async (req, res) => {
+    store = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+        
         return await this.createUserUC.handleCreate(req, res)
     }
 
-    updateUser = async (req, res) => {
+    update = async (req, res) => {
+        const responsePermission = await this.checkPermission(req, this.getCurrentFunctionName())
+        if (!responsePermission.validate) return error(res, responsePermission.message)
+        
         const id = parseInt(req.params.id)
         return await this.updateUserUC.handleUpdate(id, req, res)
     }
